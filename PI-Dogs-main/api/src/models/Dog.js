acce7fbf398a2511897aeaff10314b901b23de6b
Azul <azul235arangui@gmail.com>
2023-04-28 11:40:45 -0300

    Models

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -4,9 +4,48 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {        
+        len: [4, 150]
+      }
     },
-  });
+   
+    min_height: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    max_height: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    min_weight: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    max_weight: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue:"https://www.ibicosrl.com.ar/assets/images/17/default.png",
+      validate: {isUrl: true}
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },{timestamps: false});
+  ;
 };
