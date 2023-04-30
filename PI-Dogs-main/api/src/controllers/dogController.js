6bac944335fd1c0785c936efa0626f070518a91e
Azul <azul235arangui@gmail.com>
2023-04-30 07:47:37 -0300

    Terminar las rutas del get

---
+++ PI-Dogs-main/api/src/controllers/dogController.js
@@ -0,0 +1,67 @@
+const axios = require("axios");
+const { API_KEY } = process.env;
+const { Dog, Temperament } = require("../db");
+const getAllDogs = async () => {
+  const newApi = await axios.get(   
+    `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
+  );
+  const myInfo = await newApi.data.map((e) => {
+    return {  
+      id: e.id,  
+      name: e.name,
+      min_height: e.height.metric.split("-")[0].trim(),
+      max_height: e.height.metric.split("-").reverse()[0].trim(),
+      min_weight: e.weight.metric.split("-")[0].trim(),
+      max_weight: e.weight.metric.split("-").reverse()[0].trim(),
+      life_span: e.life_span,
+      temperament: e.temperament,
+      image: e.image.url,
+      created: false
+    };
+  });
+
+  return myInfo;
+};
+const dataInfo = async () => {//buscar perros y temperamentos en la db
+  return await Dog.findAll({//filtra resultados y ordena datos, busca
+    include: [
+      {
+        model: Temperament,
+        attributes: ["name"],
+        through: {//no en la tabla intermedia 
+          attributes: [],
+        },
+      },
+    ],
+  });
+};
+const allInfo = async () => {// trae la info de la api y de la db
+  // lista completa de perros 
+  const searchApi = await getAllDogs();
+  let searchDb = await dataInfo();
+  searchDb = await searchDb.map((e) => {
+    return {
+      id: e.id,
+      name: e.name,
+      min_height: e.min_height,
+      max_height: e.max_height,
+      min_weight: e.min_weight,
+      max_weight: e.max_weight,
+      life_span: e.life_span,
+      image: e.image,
+      temperament: e.temperaments    
+      
+        .map((e) => {      
+          return e.name;           
+        })  
+        .join(", "),// separa el una lista de elementos con una coma, toma una lista[] y devuelve una cadena 
+      createdInDb: e.createdInDb,
+    };
+  });
+  const totalInfo = [...searchApi, ...searchDb];
+  return totalInfo;
+};
+module.exports = {
+  getAllDogs,
+  allInfo,
+}; 
